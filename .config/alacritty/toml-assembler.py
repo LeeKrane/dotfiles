33e42e120f99cad49aed8cdaf19236e4405e586b
krane <christian.kranabetter@protonmail.com>
2024-07-18 21:56:49 +0200

    add alacritty config

---
+++ .config/alacritty/toml-assembler.py
@@ -0,0 +1,26 @@
+import toml
+
+
+def merge(paths):
+    merged = {}
+
+    for p in paths:
+        with open(p, "r") as f:
+            merged.update(toml.load(f))
+    return merged
+
+
+def save(conf, out_path):
+    with open(out_path, "w") as f:
+        toml.dump(conf, f)
+
+
+out_path = "./alacritty.toml"
+
+paths = [
+    "./configs/basic.toml",
+    "./configs/keybindings.toml",
+    "./themes/catppuccin-mocha.toml",
+]
+
+save(merge(paths), out_path)
