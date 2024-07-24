30f0bd5360fa27768a7df31618d151638a0f7654
krane <christian.kranabetter@protonmail.com>
2024-07-24 10:01:12 +0200

    [sys-init] fix color outputs

--- .sys-init-fedora.sh
+++ .sys-init-fedora.sh
@@ -3,19 +3,19 @@
 NC="\033[0m"
 BLUE="\033[1;34m"
 
-echo "${BLUE}Always clone the dotfiles repository as ~/.dotfiles"
-echo "Run this script without sudo. Rebos won't work if this script is run as sudo.${NC}"
+echo -e "${BLUE}Always clone the dotfiles repository as ~/.dotfiles"
+echo -e "Run this script without sudo. Rebos won't work if this script is run as sudo.${NC}"
 echo
 echo
 
 # initial programs
-echo "${BLUE}Installing initial programs needed for system setup:${NC}"
+echo -e "${BLUE}Installing initial programs needed for system setup:${NC}"
 sudo dnf -y install stow cargo
 echo
 echo
 
 # dotfiles
-echo "${BLUE}Linking your dotfiles via stow...${NC}"
+echo -e "${BLUE}Linking your dotfiles via stow...${NC}"
 cd ~/.dotfiles
 stow --adopt .
 git reset --hard
@@ -25,7 +25,7 @@ echo
 echo
 
 # important repositories and keys for rebos
-echo "${BLUE}Adding needed dnf repositories and rpm keys:${NC}"
+echo -e "${BLUE}Adding needed dnf repositories and rpm keys:${NC}"
 # terra
 sudo dnf -y config-manager --add-repo https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
 sudo dnf -y --refresh upgrade
@@ -45,20 +45,20 @@ echo
 echo
 
 # rebos for remaining programs
-echo "${BLUE}Installing Rebos for the remaining system packages:${NC}"
+echo -e "${BLUE}Installing Rebos for the remaining system packages:${NC}"
 cargo install rebos
 echo "export PATH='/home/$USER/.cargo/bin/:$PATH'" > .krane-rc/local-paths
 source ~/.bashrc
 echo
-echo "${BLUE}Installing the remaining system packages via Rebos:${NC}"
+echo -e "${BLUE}Installing the remaining system packages via Rebos:${NC}"
 rebos setup
 rebos gen commit "[sys-init] automatic initial base system configuration"
 rebos gen current build
 echo
-echo "${BLUE}System initialization is complete! Please manually install the following programs:"
+echo -e "${BLUE}System initialization is complete! Please manually install the following programs:"
 echo
 echo " - JetBrains IntelliJ"
 echo " - JetBrains WebStorm"
 echo
 echo "(If using Nobara, remember to only update via the 'Update System' program provided by Glorious Egroll)"
-echo "Exiting...${NC}"
+echo -e "Exiting...${NC}"
