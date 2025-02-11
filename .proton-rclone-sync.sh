7382ee767cf2bd9c1e09e8d954e9ee151f7663b3
krane <christian.kranabetter@protonmail.com>
2025-02-11 12:57:54 +0100

    [sync] merge rclone mounts

--- .proton-rclone-sync.sh
+++ .proton-rclone-sync.sh
@@ -3,13 +3,4 @@ rclone mount \
 	/@protondrive/ \
 	--daemon \
 	--vfs-cache-mode full \
-	--poll-interval 10m \
-	--exclude "clips_$(hostname)"
-
-rclone mount \
-	ProtonDrive:/clips_$(hostname) \
-	~/Videos/clips/ \
-	--daemon \
-	--vfs-cache-mode full \
 	--poll-interval 10m
-
