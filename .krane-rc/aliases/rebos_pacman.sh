42d2d6200195082158ef688754f379b821bba2da
krane <chris@krane.dev>
2025-04-13 21:41:55 +0200

    [bash, zsh] fix new rebos sys pkg aliases

--- .krane-rc/aliases/rebos_pacman.sh
+++ .krane-rc/aliases/rebos_pacman.sh
@@ -1,4 +1,4 @@
-alias rebosadd="sudo pacman -S"
+alias rebosadd="sudo pacman -S --noconfirm"
 alias rebosremove="sudo pacman -R"
 alias rebossync="sudo pacman -Sy"
 alias rebosupgrade="sudo pacman -Syu"
