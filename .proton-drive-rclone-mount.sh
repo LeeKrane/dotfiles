63bd70a694178f209fee299e7720fa678a618fd6
krane <christian.kranabetter@protonmail.com>
2025-04-22 13:15:29 +0200

    [rclone] add auto mount script info if 2fa expired

--- .proton-drive-rclone-mount.sh
+++ .proton-drive-rclone-mount.sh
@@ -3,16 +3,22 @@
 set +m
 
 # Mount ProtonDrive
-rclone mount \
+output=$(rclone mount \
 	ProtonDrive:/ \
 	/@protondrive/ \
 	--daemon \
 	--vfs-cache-mode full \
-	--poll-interval 10m
+	--poll-interval 10m 2>&1)
 
 # Check exit status and send notification
 if [ $? -eq 0 ]; then
 	notify-send "Proton Drive Mount" "Mounting was successful!"
 else
-	notify-send "Proton Drive Mount" "Mounting failed!" --urgency=critical
+	if [[ "$output" == *"2fa: Incorrect login credentials."* ]]; then
+		extra_message=" (2fa expired)"
+	else
+		extra_message=""
+	fi
+
+	notify-send "Proton Drive Mount" "Mounting failed!$extra_message" --urgency=critical
 fi
